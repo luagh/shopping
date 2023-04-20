4328d8389ac5f033f0bf961fb3ac87eb21b00388
laugh <laugh@email.com>
2023-04-20 18:04:56 +0800

    路由配置和404页面捕获

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,19 @@ import {
     createRouter, createWebHistory
 } from 'vue-router'
 
-const routes = []
+import Index from '~/pages/index.vue'
+import About from '~/pages/about.vue'
+import NotFound from '~/pages/404.vue'
+// 配置路由
+const routes = [{
+    path: '/',
+    component: Index
+}, {
+    path: '/about',
+    component: About
+}
+    , { path: '/:pathMatch(.*)*', name: 'NotFound', component: NotFound },
+]
 
 const router = createRouter({
     history: createWebHistory(),

b92163509a8635821617fc0bb075bea4f4ada695
laugh <laugh@email.com>
2023-04-20 20:58:08 +0800

    登录页的开发

--- src/main.js
+++ src/main.js
@@ -4,9 +4,12 @@ import 'element-plus/dist/index.css'
 import App from './App.vue'
 import router from './router'
 import 'virtual:windi.css'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 const app = createApp(App)
 app.use(router)
 
 app.use(ElementPlus)
-
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component)
+}
 app.mount('#app')
--- src/pages/login.vue
+++ src/pages/login.vue
@@ -1,5 +1,5 @@
 <template>
-    <el-row style="min-height: 100vh;" class="bg-gray-50">
+    <el-row style="min-height: 100vh;" class="bg-gray-10">
         <el-col :span="24" class="flex items-center justify-center flex-col">
             <h2 class="font-bold text-3xl text-gray-800">欢迎您</h2>
             <div class="flex items-center justify-center my-5  text-gray-300 space-x-2">
@@ -9,22 +9,37 @@
             </div>
             <el-form :model="form" class="w-[250px]">
                 <el-form-item>
-                    <el-input class="w-[250px]" size="large" v-model="form.username" placeholder="请输入用户名"></el-input>
+                    <el-input class="w-[250px]" size="large" v-model="form.username" placeholder="请输入用户名">
+                        <template #prefix>
+                            <el-icon>
+                                <UserFilled />
+                            </el-icon>
+                        </template>
+
+                    </el-input>
                 </el-form-item>
                 <el-form-item>
-                    <el-input class="w-[250px]" size="large" v-model="form.password" placeholder="请输入密码"></el-input>
+                    <el-input class="w-[250px]" size="large" v-model="form.password" placeholder="请输入密码">
+                        <template #prefix>
+                            <el-icon>
+                                <el-icon>
+                                    <Lock />
+                                </el-icon>
+                            </el-icon>
+                        </template>
+
+                    </el-input>
                 </el-form-item>
                 <el-form-item>
                     <el-button class="w-[250px]" round color="#626aef" type="primary" @click="onSubmit"> 登 录</el-button>
                 </el-form-item>
             </el-form>
         </el-col>
-
-
     </el-row>
 </template>
 <script  setup>
 import { reactive } from 'vue'
+
 
 // do not use same name with ref
 const form = reactive({
@@ -37,3 +52,5 @@ const onSubmit = () => {
     console.log('submit!')
 }
 </script>
+
+<style scoped></style>

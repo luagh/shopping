ddcddc6e2f09a321eac8b91783548b0b49a3e730
laugh <laugh@email.com>
2023-04-22 15:49:06 +0800

    使用keep-alive页面缓存

--- src/layouts/admin.vue
+++ src/layouts/admin.vue
@@ -6,7 +6,11 @@
          <el-container>
              <el-main>
                  <FTaglist/>
-                 <router-view></router-view>
+                 <router-view v-slot="{Component}">
+                <keep-alive :max="10">
+                    <component :is="Component"></component>
+                </keep-alive>
+                </router-view>
              </el-main>
              <el-aside order="2" style="flex-grow: 0; flex-shrink: 0; flex-basis: 250px;">
                  <FMenu/>

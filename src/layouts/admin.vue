a1739e5abf92c9af4b352242821021e2dc224840
laugh <laugh@email.com>
2023-04-22 15:55:39 +0800

    点击切换时动画过渡效果

--- src/layouts/admin.vue
+++ src/layouts/admin.vue
@@ -7,9 +7,12 @@
              <el-main>
                  <FTaglist/>
                  <router-view v-slot="{Component}">
-                <keep-alive :max="10">
+                    <transition name="fade">
+                        <keep-alive :max="10">
                     <component :is="Component"></component>
                 </keep-alive>
+                    </transition>
+                
                 </router-view>
              </el-main>
              <el-aside order="2" style="flex-grow: 0; flex-shrink: 0; flex-basis: 250px;">
@@ -23,3 +26,31 @@ import FHeader from './components/FHeader.vue'
 import FMenu from './components/FMenu.vue'
 import FTaglist from './components/FTaglist.vue'
 </script>
+<style>
+.el-aside{
+    transition: all 0.2s;
+}
+
+.fade-enter-to{
+    opacity: 1;
+}
+.fade-leave-from {
+    opacity: 1;
+}
+
+.fade-enter-from{
+    opacity: 0;
+
+}
+.fade-leave-to {
+  opacity: 0;
+}
+.fade-enter-active{
+    transition-delay: 0.3s;
+
+}
+.fade-leave-active{
+    transition: all 0.3s;
+    
+}
+</style>

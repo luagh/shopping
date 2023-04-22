814df24ece065d7ee8d34273282dfb71d69fdeb8
laugh <laugh@email.com>
2023-04-22 19:54:08 +0800

    首页图形数据的显示

--- src/layouts/components/FHeader.vue
+++ src/layouts/components/FHeader.vue
@@ -1,38 +1,34 @@
 <template>
-    <div class="f-header">
-        <el-dropdown class="dropdown" @command="handleCommand">
-        <span class="flex items-center text-light-50 ">
-       <el-avatar class="mr-2" size="25" :src="$store.state.user.avatar"/>
-       {{ $store.state.user.username}}
-       <el-icon class="el-icon--right">
-         <arrow-down />
-       </el-icon>
-       </span>
-        <template #dropdown>
-            <el-dropdown-menu >
-            <el-dropdown-item command="rePassword" >修改密码</el-dropdown-item>
-            <el-dropdown-item command="logout" @click="handleLogout">退出登录</el-dropdown-item>
-            </el-dropdown-menu>         
-        </template>
-     </el-dropdown>
-        
-        <el-tooltip
-          effect="dark"
-          content="全屏"
-          placement="bottom">
-          <el-icon class="icon-btn" @click="toggle">
-            <full-screen v-if="!isFullscreen" />
-            <aim v-else/>
+  <div class="f-header">
+    <el-dropdown class="dropdown" @command="handleCommand">
+      <span class="flex items-center text-light-50 ">
+        <el-avatar class="mr-2" size="25" :src="$store.state.user.avatar" />
+        {{ $store.state.user.username }}
+        <el-icon class="el-icon--right">
+          <arrow-down />
         </el-icon>
-        </el-tooltip>
-        
-        <el-tooltip
-          effect="dark"
-          content="刷新"
-          placement="bottom">
-        <el-icon class="icon-btn" @click="handleRefresh"><refresh /></el-icon>
-        </el-tooltip>
-        <div class="mx-auto flex items-center">
+      </span>
+      <template #dropdown>
+        <el-dropdown-menu>
+          <el-dropdown-item command="rePassword">修改密码</el-dropdown-item>
+          <el-dropdown-item command="logout" @click="handleLogout">退出登录</el-dropdown-item>
+        </el-dropdown-menu>
+      </template>
+    </el-dropdown>
+
+    <el-tooltip effect="dark" content="全屏" placement="bottom">
+      <el-icon class="icon-btn" @click="toggle">
+        <full-screen v-if="!isFullscreen" />
+        <aim v-else />
+      </el-icon>
+    </el-tooltip>
+
+    <el-tooltip effect="dark" content="刷新" placement="bottom">
+      <el-icon class="icon-btn" @click="handleRefresh">
+        <refresh />
+      </el-icon>
+    </el-tooltip>
+    <div class="mx-auto flex items-center">
       <el-tooltip effect="dark" content="GIN后台" placement="bottom">
         <span class="logo flex items-center justify-center">
           <el-icon class="mr-1"><eleme-filled /></el-icon>
@@ -40,66 +36,66 @@
         </span>
       </el-tooltip>
     </div>
-       <div class="ml-auto flex items-center">
-        <el-icon class="icon-btn" @click="$store.commit('handleAsideWidth')">
-            <Expand v-if="$store.state.asideWidth == '250px'"/>
-            <fold v-else />
-        </el-icon>
-       
-       </div>
-       
+    <div class="ml-auto flex items-center">
+      <el-icon class="icon-btn" @click="$store.commit('handleAsideWidth')">
+        <Expand v-if="$store.state.asideWidth == '250px'" />
+        <fold v-else />
+      </el-icon>
+
     </div>
-  
-    <FormDrawer ref="formDrawerRef" title="修改密码" destroyOnClose @submit="onSubmit">
-        
-    <el-form ref="formRef" :rules="rules" :model="form" label-width="80px" size="small" >
-        <el-form-item prop="oldpassword" label="旧密码">
-                <el-input v-model="form.oldpassword" placeholder="请输入旧密码">
-                </el-input>
-            </el-form-item>
-            <el-form-item prop="password" label="新密码">
-                <el-input type="password" v-model="form.password" placeholder="请输入新密码" show-password>
-                </el-input>
-            </el-form-item>
-            <el-form-item prop="repassword" label="确认密码">
-                <el-input type="repassword" v-model="form.repassword" placeholder="请输入确认密码" show-password>
-                </el-input>
-            </el-form-item>
-</el-form>
-    
-    </FormDrawer>
+
+  </div>
+
+  <FormDrawer ref="formDrawerRef" title="修改密码" destroyOnClose @submit="onSubmit">
+
+    <el-form ref="formRef" :rules="rules" :model="form" label-width="80px" size="small">
+      <el-form-item prop="oldpassword" label="旧密码">
+        <el-input v-model="form.oldpassword" placeholder="请输入旧密码">
+        </el-input>
+      </el-form-item>
+      <el-form-item prop="password" label="新密码">
+        <el-input type="password" v-model="form.password" placeholder="请输入新密码" show-password>
+        </el-input>
+      </el-form-item>
+      <el-form-item prop="repassword" label="确认密码">
+        <el-input type="repassword" v-model="form.repassword" placeholder="请输入确认密码" show-password>
+        </el-input>
+      </el-form-item>
+    </el-form>
+
+  </FormDrawer>
 </template>
 <script setup>
 
-import {useFullscreen} from '@vueuse/core'
+import { useFullscreen } from '@vueuse/core'
 import FormDrawer from "~/components/FormDrawer.vue"
-import { useRepassword,useLogout } from '~/composables/useManager'
+import { useRepassword, useLogout } from '~/composables/useManager'
 //isFullscreen 是否全屏 //toggle切花全屏
 const { isFullscreen, toggle } = useFullscreen()
 
 const { formDrawerRef,
-        form,
-        rules,
-        formRef,
-        onSubmit,
-        openRePassWordForm }= useRepassword()
+  form,
+  rules,
+  formRef,
+  onSubmit,
+  openRePassWordForm } = useRepassword()
 
-const {handleLogout} = useLogout()
+const { handleLogout } = useLogout()
 
 //
 const handleCommand = (c) => {
-    //console.log(c);
-    switch (c) {
-        case "logout":
-            handleLogout()
-            break;
-        case "rePassword":
-            // showDrawer.value =true
-            openRePassWordForm()
-            break;
-        default:
-            break;
-    }
+  //console.log(c);
+  switch (c) {
+    case "logout":
+      handleLogout()
+      break;
+    case "rePassword":
+      // showDrawer.value =true
+      openRePassWordForm()
+      break;
+    default:
+      break;
+  }
 
 }
 
@@ -107,38 +103,45 @@ const handleCommand = (c) => {
 
 //刷新
 function handleRefresh() {
-    location.reload()
+  location.reload()
 }
 
 </script>
 <style>
-.f-header{
-    @apply flex items-center bg-indigo-500 text-light-50 fixed top-0 left-0 right-0;
-   height:64px;
+.f-header {
+  @apply flex items-center bg-indigo-500 text-light-50 fixed top-0 left-0 right-0;
+  height: 64px;
+  z-index: 1000;
 }
-.logo{
-    width:150px;
-@apply flex justify-center items-center text-xl font-thin;
+
+.logo {
+  width: 150px;
+  @apply flex justify-center items-center text-xl font-thin;
 }
-.icon-btn{
-    @apply flex justify-center items-center;
-       width:42px;
-       height:64px;
-       cursor:pointer;
-       position: relative;
+
+.icon-btn {
+  @apply flex justify-center items-center;
+  width: 42px;
+  height: 64px;
+  cursor: pointer;
+  position: relative;
 }
-.icon-btn:hover{
-@apply bg-indigo-600;
+
+.icon-btn:hover {
+  @apply bg-indigo-600;
 }
-.f-header .dropdown{
-    height:64px;
-    cursor:pointer;
-    @apply flex justify-center items-center mx-5;
+
+.f-header .dropdown {
+  height: 64px;
+  cursor: pointer;
+  @apply flex justify-center items-center mx-5;
 }
+
 .dialog-footer {
   display: flex;
   justify-content: center;
 }
+
 .mx-auto {
   @apply mx-auto;
 }
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -73,13 +73,25 @@
             </el-col>
         </el-row>
         <IndexNavs />
+        <el-row :gutter="20" class="mt-5">
+            <el-col :span="12" :offset="0">
+                <IndexChart />
+            </el-col>
+            <el-col :span="12" :offset="0">
+                <IndexCard title="店铺及商品提示" tip="店铺及商品提示" :btns="goods" class="mb-3" />
+                <IndexCard title="交易提示" tip="需要立即处理的订单" :btns="order" />
+            </el-col>
+        </el-row>
+
     </div>
 </template>
 <script setup>
-import { getStatistics1 } from "~/api/index"
+import { getStatistics1, getStatistics2 } from "~/api/index"
 import { ref } from "vue"
 import CountTo from "~/components/CountTo.vue"
 import IndexNavs from "~/components/IndexNavs.vue"
+import IndexChart from "~/components/IndexChart.vue"
+import IndexCard from "~/components/IndexCard.vue"
 
 const panels = ref([])
 getStatistics1()
@@ -87,6 +99,12 @@ getStatistics1()
         panels.value = res.panels
         console.log(panels.value);
     });
+const goods = ref([])
+const order = ref([])
 
+getStatistics2().then(res => {
+    goods.value = res.goods
+    order.value = res.order
+});
 </script>
 <style scoped></style>

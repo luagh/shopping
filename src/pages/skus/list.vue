50c86f69ff5a9701aa264cf55a08d5c756494767
laugh <laugh@email.com>
2023-04-24 20:50:35 +0800

    规格管理新增修改删除数据的实现

---
+++ src/pages/skus/list.vue
@@ -0,0 +1,121 @@
+<template>
+    <el-card shadow="never">
+        <!-- 新增|刷新 -->
+        <div class="flex items-center justify-between mb-4">
+            <el-button type="primary" size="small" @click="handlecreate">新增</el-button>
+            <el-tooltip effect="dark" content="刷新数据" placement="top">
+                <el-button text @click="getData">
+                    <el-icon size="20">
+                        <Refresh />
+                    </el-icon>
+                </el-button>
+            </el-tooltip>
+        </div>
+
+        <el-table :data="tableData" stripe style="width: 100%" v-loading="loading">
+            <el-table-column prop="name" label="规格名称" />
+            <el-table-column prop="default" label="规格值" />
+            <el-table-column prop="order" label="排序" />
+            <el-table-column label="状态" width="120">
+                <template #default="{ row }">
+                    <el-switch :modelValue="row.status" :active-value="1" inactive-value="0" :loading="row.statusLoading"
+                        :disabled="row.super == 1" @change="handleStatuschange($event, row)">
+                    </el-switch>
+                </template>
+            </el-table-column>
+            <el-table-column label="操作" width="250px" align="center">
+                <template #default="scope">
+                    <el-button type="primary" size="small" text @click="handleEdit(scope.row)">修改</el-button>
+                    <el-popconfirm title="是否要删除" confirmbuttontext="确认" cancelbuttontext="取消"
+                        @confirm="handleDelete(scope.row.id)">
+                        <template #reference>
+                            <el-button text type="primary" size="small">删除
+                            </el-button>
+                        </template>
+                    </el-popconfirm>
+                </template>
+            </el-table-column>
+        </el-table>
+        <div class="flex items-center justify-center mt-5">
+            <el-pagination :pager-count="11" layout="prev,pager, next" :total="total" :current-page="currentPage"
+                :page-size="limit" @current-change="getData" />
+        </div>
+        <FormDrawer ref="formDrawerRef" :title="drawerTitle" @submit="handleSubmit">
+            <el-form :model="form" ref="formRef" :rules="rules" label-width="80px" :inline="false">
+                <el-form-item label="规格名称" prop="name">
+                    <el-input v-model="form.name" placeholder="规格名称"></el-input>
+                </el-form-item>
+                <el-form-item label="配许" prop="order">
+                    <el-input-number v-model="form.order" :min="0" :max="1000">
+                    </el-input-number>
+                </el-form-item>
+                <el-form-item label="状态" prop="status">
+                    <el-switch v-model="form.status" :active-value="1" inactive-value="0">
+                    </el-switch>
+                </el-form-item>
+                <el-form-item label="规格值" prop="default">
+                    <TagInput v-model="form.default"></TagInput>
+                </el-form-item>
+            </el-form>
+        </FormDrawer>
+
+
+    </el-card>
+</template>
+<script setup>
+
+import {
+    getSkusList, createSkus, updateSkus, deleteSkus,
+    updateSkusStatus
+} from "~/api/skus.js"
+import FormDrawer from "~/components/FormDrawer.vue";
+import { useInitTable, useInitForm } from '~/composables/useCommon.js'
+import { ref } from "vue"
+
+import { toast } from "~/composables/util.js"
+const {
+    tableData,
+    loading,
+    currentPage,
+    total,
+    limit,
+    getData,
+    handleDelete,
+    handleStatuschange
+} = useInitTable({
+    getList: getSkusList,
+    delete: deleteSkus,
+    updateStatus: updateSkusStatus
+})
+const {
+    formDrawerRef,
+    formRef,
+    form,
+    rules,
+    drawerTitle,
+    handleSubmit,
+    handlecreate,
+    handleEdit } = useInitForm({
+        form: {
+            name: '',
+            default: '',
+            default: "",
+            order: 50
+
+        },
+        rules: {
+            name: [{ required: true, message: '规格名称不能为空', trigger: 'blur' },],
+            default: [{ required: true, message: '规格值不能为空', trigger: 'blur' },]
+        },
+        getData,
+        update: updateSkus,
+        create: createSkus
+    })
+
+const handleTreeCheck = (...e) => {
+    const { checkedKeys, halfCheckedKeys } = e[1]
+    ruleIds.value = [...checkedKeys, ...halfCheckedKeys]
+}
+
+</script>
+<style></style>

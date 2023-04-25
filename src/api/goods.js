54d0eb96aaf4cfcb59b961218be7666fe6e8568f
laugh <laugh@email.com>
2023-04-25 11:00:16 +0800

    商品列表数据展示

---
+++ src/api/goods.js
@@ -0,0 +1,34 @@
+import axios from '~/axios'
+import { queryParams } from '~/composables/util.js'
+
+
+
+// 
+export function getGoodsList(page, query = {}) {
+
+    let r = queryParams(query)
+    return axios.get(`/admin/goods/${page}${r}`)
+}
+
+// 修改
+export function updateGoodsStatus(ids, status) {
+    return axios.post(`/admin/goods/changestatus`, {
+        ids, status
+    })
+}
+
+// 新增修改删除
+export function createGoods(data) {
+    return axios.post(`/admin/goods`, data)
+
+}
+
+export function updateGoods(id, data) {
+    return axios.post(`/admin/goods/${id}`, data)
+}
+export function deleteGoods(ids) {
+    return axios.post(`/admin/goods/delete_all`, {
+        ids
+    })
+}
+
--- src/composables/util.js
+++ src/composables/util.js
@@ -34,6 +34,21 @@ export function showPrompt(tip, value = "") {
     )
 }
 
+//将query)对象转成url参数
+export function queryParams(query) {
+    let q = []
+    for (const key in query) {
+        if (query[key]) {
+            q.push(`${key}=${encodeURIComponent(query[key])}`)
+        }
+    }
+    let r = q.join("&")
+    r = r ? ("?" + r) : ""
+
+    return r
+}
+
+
 //显示全屏1 oading
 export function showFullLoading() {
     nprogress.start()

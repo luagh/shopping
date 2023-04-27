6ec39b52438054b6af7144e961005c2f70ed482e
laugh <laugh@email.com>
2023-04-27 17:10:28 +0800

    系统设置模块基础设置数据的显示

---
+++ src/api/sysconfig.js
@@ -0,0 +1,8 @@
+import axios from '~/axios'
+
+export function getSysconfig() {
+    return axios.get(`/admin/sysconfig`)
+}
+export function setSysconfig(data) {
+    return axios.post(`/admin/sysconfig`, data)
+}
